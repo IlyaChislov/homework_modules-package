a647dbf3216dedfb5068df0d86f25b1acae62c3a
Ilya_Ch <ilia.spartak@yandex.ru>
2024-01-28 23:21:12 +0300

    first

---
+++ application/DB/people.py
@@ -0,0 +1,4 @@
+import datetime
+def get_employees():
+    print(datetime.date.today())
+    return "В вашей организации 15 000 сотрудников"
---
+++ application/salary.py
@@ -0,0 +1,4 @@
+import datetime
+def calculate_salary():
+    print(datetime.date.today())
+    return "Ваша зарплата за текущий месяц с учётом премии: 116 000 рублей"
